ab13cf38c2ba3b8c0474f5b02e21d460fade96b8
unknown <devansh.juwar@coditas.com>
2026-06-12 02:04:51 +0530

    Changes Made

--- src/app/layout/initial-layout/initial-layout.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-initial-layout',
-  imports: [],
-  templateUrl: './initial-layout.html',
-  styleUrl: './initial-layout.scss',
-})
-export class InitialLayout {}
